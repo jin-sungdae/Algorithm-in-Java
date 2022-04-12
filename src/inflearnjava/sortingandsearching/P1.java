8522fd8127d56397f440b344e03ebf7ef00551b9
Sungdae Jin <sjin@c6r9s5.42seoul.kr>
2022-04-12 12:17:31 +0900

    sort

--- src/inflearnjava/sortingandsearching/P1.java
+++ src/inflearnjava/sortingandsearching/P1.java
@@ -1,4 +1,34 @@
 package inflearnjava.sortingandsearching;
 
+import java.util.*;
+
 public class P1 {
+	
+	static int[] list;
+	
+	public static void solution() {
+		int temp = 0;
+		for (int i =0 ; i < list.length; i++) {
+			for (int j = i + 1; j < list.length; j++) {
+				if (list[i] > list[j]) {
+					temp = list[i];
+					list[i] = list[j];
+					list[j] = temp;
+				}
+			}
+		}
+		for (int x : list) {
+			System.out.print(x + " ");
+		}
+	}
+	
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		list = new int[n];
+		for (int i = 0; i < n; i++) {
+			list[i] = sc.nextInt();
+		}
+		solution();
+	}
 }
--- src/inflearnjava/sortingandsearching/P2.java
+++ src/inflearnjava/sortingandsearching/P2.java
@@ -1,4 +1,34 @@
 package inflearnjava.sortingandsearching;
 
+import java.util.Scanner;
+
 public class P2 {
+	
+	static int [] list;
+	
+	public static void solution() {
+		int tmp = 0;
+		for (int i = 0; i < list.length - 1; i++) {
+			for (int j = 0; j < list.length - i - 1; j++) {
+				if (list[j] > list[j + 1]) {
+					tmp = list[j];
+					list[j] = list[j + 1];
+					list[j + 1] = tmp;
+				}
+			}
+		}
+		for (int x : list) {
+			System.out.print(x + " ");
+		}
+	}
+	
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		list = new int[n];
+		for (int i = 0; i < n; i++) {
+			list[i] = sc.nextInt();
+		}
+		solution();
+	}
 }
