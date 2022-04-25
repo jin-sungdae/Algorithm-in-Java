d6736b1394f8d0b2e2c55abb1a8bb59d19c26f1c
Sungdae Jin <sjin@c6r9s5.42seoul.kr>
2022-04-25 17:29:38 +0900

    algo ㅎㅚ전

--- src/study/DevMatching2021/P1.java
+++ src/study/DevMatching2021/P1.java
@@ -1,4 +1,34 @@
 package study.DevMatching2021;
 
+class Solution {
+    public static int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int resultCnt = 0, ch = 0;
+        for (int x : win_nums) {
+        	for (int y : lottos) {
+        		if (x == y) resultCnt++;
+        	}
+        }
+        for (int y : lottos) {
+        	if (y == 0) ch++; 
+        }
+        if (ch == 6) answer[1] = 6;
+        else answer[1] = 7 - resultCnt;
+        answer[0] = 7 - (resultCnt + ch);
+        if (resultCnt == 0 && ch == 0) {
+        	answer[0] = 6;
+        	answer[1] = 6;
+        }
+        return answer;
+    }
+}
+
 public class P1 {
+	public static void main(String []args) {
+		int [] lottos = {1,2,3,4,5,6};
+		int [] win_nums = {7,8,9,10,11,12};
+		for (int x : Solution.solution(lottos, win_nums)) {
+			System.out.print(x + " ");
+		}
+	}
 }
--- src/study/DevMatching2021/P2.java
+++ src/study/DevMatching2021/P2.java
@@ -1,4 +1,66 @@
 package study.DevMatching2021;
+import java.util.*;;
+class Solution2 {
+    public static int[] solution(int rows, int columns, int[][] queries) {
+        
+        int [][] board = new int [rows + 1][columns + 1];
+        int sum = 0, min = Integer.MAX_VALUE;
+        for (int i = 0; i < rows; i++) {
+        	for (int j = 0; j < columns; j++) {
+        		sum += 1;
+        		board[i][j] = sum;
+        	}
+        }
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int [] x : queries) {
+        	min = Integer.MAX_VALUE;
+        	int tmp1 = board[x[0] - 1][x[1] - 1];
+        	int tmp2 = 0;
+        	min = Math.min(min, tmp1);
+        	for (int i = x[0] - 1; i < x[2] - 1; i++) {
+        		board[i][x[1] - 1] = board[i + 1][x[1] - 1];
+        		tmp2 = board[i][x[1] - 1];
+        		min = Math.min(min, tmp2);
+        	}
+
+        	for (int i = x[1] - 1; i < x[3] - 1;  i++) {
+        		board[x[2] - 1][i] = board[x[2] - 1][ i + 1];
+        		tmp2 = board[x[2] - 1][i];
+        		min = Math.min(min, tmp2);
+        	}
+
+        	for (int i = x[2] -  1; i > x[0] - 1; i--) {
+        		board[i][x[3] - 1] = board[i - 1][x[3] - 1];
+        		tmp2 = board[i][x[3] - 1];
+        		min = Math.min(min, tmp2);
+        	}
+
+        	for (int i = x[3] -  1; i > x[1] - 1; i--) {
+        		board[x[0] - 1][i] = board[x[0] - 1][i - 1];
+        		tmp2 = board[x[0] - 1][i];
+        		min = Math.min(min, tmp2);
+        	}
+        	
+        	board[x[0] - 1][x[1]] = tmp1;
+    		arr.add(min);
+
+        }
+        
+
+        int []answer = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+        	answer[i] = arr.get(i).intValue();
+        }
+        return answer;
+    }
+}
 
 public class P2 {
+	public static void main(String []args) {
+		int [][] queries = {{1,1,2,2},{1,2,2,3},{2,1,3,2},{2,2,3,3}};
+		for (int x : Solution2.solution(3, 3, queries)) {
+			System.out.print(x + " ");
+		}
+	}
 }
