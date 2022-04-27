c3b1389e37984e939e1e53b582204cfaaef26cce
Sungdae Jin <sjin@c6r9s5.42seoul.kr>
2022-04-27 18:38:52 +0900

    devmatching 2021 p1

--- src/study/DevMatching2021/P3.java
+++ src/study/DevMatching2021/P3.java
@@ -1,4 +1,48 @@
 package study.DevMatching2021;
 
+class Solution3 {
+    public static String solution(String new_id) {
+        String answer = "";
+        new_id = new_id.toLowerCase();
+        answer = new_id.replaceAll("[^a-z0-9-._]", "");
+
+//        while (answer.indexOf("..") > -1)
+//        	answer = answer.replace("..", ".");
+        for (int i = 0 ; i < answer.length() + 10; i++) {
+        	answer = answer.replace("..", ".");
+        }
+        System.out.println(answer);
+
+        if (answer.length() != 0) {
+            if (answer.charAt(0) == '.' && answer.length() > 1)
+                answer = answer.substring(1);
+            if (answer.length() > 0 && answer.charAt(answer.length() - 1) == '.')
+                answer = answer.substring(0, answer.length() - 1);
+        }
+//        System.out.println(answer);
+         if (answer.length() == 1 && answer.charAt(0) == '.')
+             answer = "";
+        System.out.println(answer);
+
+        if (answer.length() == 0) 
+        	answer = "a";
+        if (answer.length() >= 16) {
+        	answer = answer.substring(0, 15);
+        	if (answer.charAt(14) == '.')
+            	answer = answer.substring(0, 14);
+            
+        }
+        while (answer.length() < 3)
+            answer += answer.charAt(answer.length() - 1);
+        return answer;
+    }
+}
 public class P3 {
+	public static void main(String []args) {
+		String str = "................b";
+		System.out.println(str.length());
+		//System.out.println(Solution3.solution("=.="));
+		System.out.println(Solution3.solution("................b"));
+	
+	}
 }
--- src/study/DevMatching2021/P4.java
+++ src/study/DevMatching2021/P4.java
@@ -1,4 +1,25 @@
 package study.DevMatching2021;
 
+import java.util.*;
+
+class Solution4 {
+	static int [] alpa ;
+    public static String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        ArrayList<String> arr = new ArrayList<>();
+        
+        return answer;
+    
+}
+}
+
+
 public class P4 {
+	public static void main(String []args) {
+		String [] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+		int [] course = {2,3,4};
+		
+		System.out.println(Solution4.solution(orders, course));
+
+	}
 }
