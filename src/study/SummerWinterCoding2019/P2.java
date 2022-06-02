c6d112d5618f3027325f4b65bd2e49dee6bafe63
jin-sungdae <makonle@naver.com>
2022-06-02 17:13:47 +0900

    summerwintercoding2019 P2 and P3

--- src/study/SummerWinterCoding2019/P2.java
+++ src/study/SummerWinterCoding2019/P2.java
@@ -1,18 +1,65 @@
 package study.SummerWinterCoding2019;
 
+import java.util.*;
+
+class Node{
+	int idx;
+	int cost;
+	Node(int idx, int cost){
+		this.idx = idx;
+		this.cost = cost;
+	}
+}
+
 class Solution2 {
     public static int solution(int N, int[][] road, int K) {
         int answer = 0;
-        
+        ArrayList<ArrayList<Node>> board = new ArrayList<ArrayList<Node>>();
+        for (int i = 0; i < N + 1; i++) 
+        	board.add(new ArrayList<>());
+        for (int i = 0; i < road.length; i++) {
+        	board.get(road[i][0]).add(new Node(road[i][1], road[i][2]));
+        	board.get(road[i][1]).add(new Node(road[i][0], road[i][2]));
+        }
 
-        return answer;
+        boolean[] visited = new boolean[N + 1];
+        int[] dist = new int[N + 1];
+        for (int i = 0; i < N + 1; i++) 
+            dist[i] = Integer.MAX_VALUE;
+        	
+        dist[1] = 0;
+        for (int i = 0; i < N; i++) {
+        	int nodeValue = Integer.MAX_VALUE;
+        	int nodeIdx = 0;
+        	for (int j = 1; j < N + 1; j++) {
+        		if (!visited[j] && dist[j] < nodeValue) {
+        			nodeValue = dist[j];
+        			nodeIdx = j;
+        		}
+        	}
+        		
+        	visited[nodeIdx] = true;
+        	for (int j = 0; j < board.get(nodeIdx).size(); j++) {
+        		Node adjNode = board.get(nodeIdx).get(j);
+        		if (dist[adjNode.idx] > dist[nodeIdx] + adjNode.cost)
+        			dist[adjNode.idx] = dist[nodeIdx] + adjNode.cost;
+        	}
+        }
+      
+        for (int t = 1; t < N + 1; t++) {
+        	if (dist[t] <= K)
+        		answer++;
+        }
+
+		return answer;
     }
 }
 
 public class P2 {
 	public static void main(String []args) {
 		int [][] road = {{1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}};
-		
+		int [][] road2 = {{1,2,1},{1,3,2},{2,3,2},{3,4,3},{3,5,2},{3,5,3},{5,6,1}};
 		System.out.println(Solution2.solution(5, road, 3));
+		//System.out.println(Solution2.solution(6, road2, 4));
 	}
 }
--- src/study/SummerWinterCoding2019/P3.java
+++ src/study/SummerWinterCoding2019/P3.java
@@ -1,18 +1,52 @@
 package study.SummerWinterCoding2019;
 
+import java.util.*;
+
 class Solution3 {
     public static int[] solution(int n, String[] words) {
-        int[] answer = {};
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
-        System.out.println("Hello Java");
-
+        int[] answer = new int[2];
+        String member = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < words.length; i++) {
+        	if (map.containsKey(words[i]) || i != 0 && member.charAt(member.length() - 2) != words[i].charAt(0)) {
+        		answer[0] = i % n + 1;
+        		answer[1] = i / n + 1;
+        		break;
+        	}
+        	map.put(words[i], map.getOrDefault(words[i], 0) + 1);
+        	member += words[i] + " ";
+        }
         return answer;
     }
 }
 
 public class P3 {
 	public static void main(String []args) {
+		String []words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+		for (int x : Solution3.solution(3, words)) {
+			System.out.print(x + " ");
+		}
 		
+//		String []words1 = {"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
+//		for (int x : Solution3.solution(5, words1)) {
+//			System.out.print(x + " ");
+//		}
+//		
+//		String []words2 = {"hello", "one", "even", "never", "now", "world", "draw"};
+//		for (int x : Solution3.solution(2, words2)) {
+//			System.out.print(x + " ");
+//		}
+		
+//		String []words3 = {"land", "dream", "mom", "mom"};
+//		for (int x : Solution3.solution(2, words3)) {
+//			System.out.println(x + " ");
+//		}
+		
+		
+//		String [] words4 = {"qwe", "eqwe", "eqwe"};
+//		for (int x : Solution3.solution(2, words4)) {
+//			System.out
+//		}
 	}
 }
