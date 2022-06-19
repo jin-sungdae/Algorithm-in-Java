69edfee5d5d9dd7b95dddff8f8f37a42cfa7f67c
jin-sungdae <makonle@naver.com>
2022-06-20 02:27:09 +0900

    weeklychallenge P7

--- src/study/weeklychallenge/P7.java
+++ src/study/weeklychallenge/P7.java
@@ -1,37 +1,60 @@
 package study.weeklychallenge;
 
+import java.util.*;
+
+class Point {
+	int x; int y;
+	Point (int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
+
 class Solution7 {
-	static int [][] board = new int[51][51];
+	static int [][] board = new int[101][101];
+	static int [][]visited = new int[101][101];
+	static int []dx = {-1, 1, 0, 0};
+	static int []dy = {0, 0, -1, 1};
+	public static void makeBoard(int x1, int y1, int x2, int y2) {
+		for (int i = x1; i <= x2; i++) {
+			for (int j = y1; j <= y2; j++) {
+				if (board[i][j] == 2) continue;
+				board[i][j] = 2;
+				if (i == x1 || i == x2 || j == y1 || j == y2) {
+					board[i][j] = 1;
+				}
+			}
+		}
+	}
+	
     public static int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         
-    	int answer = 0;
     	for (int i = 0; i < rectangle.length; i++) {
-    		for (int j = rectangle[i][0]; j <= rectangle[i][2]; j++) {
-    			board[rectangle[i][1]][j] = 1;
-    		}
-    		for (int j = rectangle[i][0]; j <= rectangle[i][2]; j++) {
-    			board[rectangle[i][3]][j] = 1;
-    		}
-    		for (int j = rectangle[i][1]; j <= rectangle[i][3]; j++) {
-    			board[j][rectangle[i][0]] = 1;
-    		}
-    		for (int j = rectangle[i][1]; j <= rectangle[i][3]; j++) {
-    			board[j][rectangle[i][2]] = 1;
-    		}
-    		
+    		makeBoard(2 * rectangle[i][0],2 * rectangle[i][1], 2 * rectangle[i][2], 2 * rectangle[i][3]);
     	}
     	
-    	board[characterY][characterX] = 99;
-    	board[itemY][itemX] = 88;
+    	bfs(characterX * 2, characterY * 2, itemX * 2, itemY * 2);
     	
-    	for (int i = 0; i < 10; i++) {
-    		for (int j = 0; j < 10; j++) {
-    			System.out.print(board[i][j] + " ");
+    	
+        return visited[itemX * 2][itemY * 2] / 2;
+    }
+    
+    public static void bfs(int px, int py, int itemx, int itemy) {
+    	Queue<Point> q = new LinkedList<>();
+    	q.add(new Point(px, py));
+    	while (!q.isEmpty()) {
+    		Point temp = q.poll();
+    		for (int i = 0; i < 4; i++) {
+    			int nx = temp.x + dx[i];
+    			int ny = temp.y + dy[i];
+    			if (nx > 0 && nx < 101 && ny > 0 && ny < 101 && board[nx][ny] == 1) {
+    				q.add(new Point(nx, ny));
+    				board[nx][ny] = 0;
+    				visited[nx][ny] = visited[temp.x][temp.y] + 1;
+    			}
+    			if (nx == itemx && ny == itemy) break;
     		}
-    		System.out.println();
     	}
-    	
-        return answer;
     }
 }
 
