fcca41e4c98cdc63b44d7d30c163800d2416edf5
jin-sungdae <makonle@naver.com>
2022-06-22 01:42:25 +0900

    wintersunmmer LV3 P1

--- src/study/summerandwintercoding2018LV3/P1.java
+++ src/study/summerandwintercoding2018LV3/P1.java
@@ -1,18 +1,77 @@
 package study.summerandwintercoding2018LV3;
 
+import java.util.*;
+
 class Solution {
-    public int solution(int n, int[] stations, int w) {
+    public static int solution(int n, int[] stations, int w) {
         int answer = 0;
+        int []apart = new int[n + 1];
+        
+        
+        int j = 0;
+ 
+        int cnt = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+//        for (int i = 1; i < n + 1; i++) {
+//        	apart[i] = i;
+//        	if (j < stations.length && (i >= stations[j] - w && i <= stations[j] + w)) {
+//        		apart[i] = 0;
+//        		if (stations[j] + w == i)
+//        			j++;
+//        	}
+//        	if (apart[i] != 0) {
+//    			cnt++;
+//    		} else {
+//    			answer += cnt / (1 + (w * 2));
+//    			if (cnt % (1 + (w * 2)) != 0) answer++;
+//    			cnt = 0;
+//    		}
+//        }
+//        
+//        for (int i = 1; i < n + 1; i++) {
+//        	apart[i] = i;
+//        	if (j < stations.length && i == stations[j] - w) {
+//        		apart[i] = 0;
+//        		if (stations[j] + w == i)
+//        			j++;
+//        	}
+//        	if (apart[i] != 0) {
+//    			cnt++;
+//    		} else {
+//    			answer += cnt / (1 + (w * 2));
+//    			if (cnt % (1 + (w * 2)) != 0) answer++;
+//    			cnt = 0;
+//    			i = stations[j] + w;
+//    			j++;
+//    		}
+//        }
+//
+//        if (cnt != 0) {
+//        	answer += cnt / (1 + (w * 2));
+//			if (cnt % (1 + (w * 2)) != 0) answer++;
+//        }
+        int p = 0;
+        for (int i = 0; i < stations.length; i++) {
+        	cnt = (stations[i] - w) - p - 1;
+        	answer += cnt / (1 + (w * 2));
+			if (cnt % (1 + (w * 2)) != 0) answer++;
+        	p = stations[i] + w;
+        }
+//        if (p != stations[stations.length -1] - w) {
+//        	answer += cnt / (1 + (w * 2));
+//			if (cnt % (1 + (w * 2)) != 0) answer++;
+//        }
+        
+        
+        
         return answer;
     }
 }
 
 public class P1 {
 	public static void main(String []args) {
-		
+		int []stations = {4, 11};
+		int []stations2 = {9};
+		System.out.println(Solution.solution(11, stations2, 1));
 	}
 }
